32dc0977725b4936346b95b5a42446440f75aad3
ra-phael <10075759+ra-phael@users.noreply.github.com>
2022-10-26 14:16:32 +0200

    use useEffect instead of useLayoutEffect

--- airdrop/frontend/src/helpers/DarkModeToggle.tsx
+++ airdrop/frontend/src/helpers/DarkModeToggle.tsx
@@ -1,34 +1,34 @@
-import { useLayoutEffect, useState } from 'react';
-import { DarkModeSwitch } from 'react-toggle-dark-mode';
+import { useEffect, useLayoutEffect, useState } from "react";
+import { DarkModeSwitch } from "react-toggle-dark-mode";
 
 export const DarkModeToggle = () => {
-	const [isDarkMode, setDarkMode] = useState(true);
+  const [isDarkMode, setDarkMode] = useState(true);
 
-	// Checks local storage for dark mode
-	useLayoutEffect(() => {
-		const darkMode = globalThis.localStorage.getItem('darkMode');
+  // Checks local storage for dark mode
+  useEffect(() => {
+    const darkMode = globalThis.localStorage.getItem("darkMode");
 
-		const isDark = darkMode && JSON.parse(darkMode);
+    const isDark = darkMode && JSON.parse(darkMode);
 
-		if (!isDark) {
-			globalThis.document.documentElement.classList.add('dark');
-		} else {
-			setDarkMode(isDark);
-			isDark
-				? globalThis.document.documentElement.classList.add('dark')
-				: globalThis.document.documentElement.classList.remove('dark');
-		}
-	}, []);
+    if (!isDark) {
+      globalThis.document.documentElement.classList.add("dark");
+    } else {
+      setDarkMode(isDark);
+      isDark
+        ? globalThis.document.documentElement.classList.add("dark")
+        : globalThis.document.documentElement.classList.remove("dark");
+    }
+  }, []);
 
-	const toggleDarkMode = (checked: boolean) => {
-		globalThis.localStorage.setItem('darkMode', JSON.stringify(checked));
-		setDarkMode(checked);
-		checked
-			? globalThis.document.documentElement.classList.add('dark')
-			: globalThis.document.documentElement.classList.remove('dark');
-	};
+  const toggleDarkMode = (checked: boolean) => {
+    globalThis.localStorage.setItem("darkMode", JSON.stringify(checked));
+    setDarkMode(checked);
+    checked
+      ? globalThis.document.documentElement.classList.add("dark")
+      : globalThis.document.documentElement.classList.remove("dark");
+  };
 
-	return (
-		<DarkModeSwitch checked={isDarkMode} onChange={toggleDarkMode} size={35} />
-	);
+  return (
+    <DarkModeSwitch checked={isDarkMode} onChange={toggleDarkMode} size={35} />
+  );
 };
