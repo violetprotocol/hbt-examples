7ed729836c3e1f46f47873afbf76747d9df04782
ra-phael <10075759+ra-phael@users.noreply.github.com>
2022-10-31 18:42:21 +0100

    wallet connect refactoring

--- airdrop/frontend/src/context/NavBar/MetaMask.tsx
+++ airdrop/frontend/src/context/NavBar/MetaMask.tsx
@@ -1,9 +1,10 @@
 import { useContext, Dispatch, SetStateAction } from "react";
 import { Web3Context } from "../Web3Context";
 import { Web3Provider } from "@ethersproject/providers";
-import { MockHBT, MockHBT__factory } from "lib/typechain-types/index";
-import { ethers } from "ethers";
-import { displayToast, toastInfo } from "src/utils/toast";
+import { MockHBT } from "lib/typechain-types/index";
+import { Signer } from "ethers";
+import { displayToast } from "src/utils/toast";
+import { getHBTContract } from "src/utils/hbtContract";
 
 declare let window: any;
 
@@ -11,11 +12,12 @@ export interface Web3 {
   contract: MockHBT;
   provider: Web3Provider;
   account: string;
+  signer: Signer;
   setWeb3?: Dispatch<SetStateAction<Web3>>;
 }
 
 export const MetaMask = () => {
-  const { account, setWeb3 } = useContext(Web3Context);
+  const { account, setWeb3, contract } = useContext(Web3Context);
 
   async function enableEth() {
     const ethereum = window.ethereum;
@@ -25,56 +27,15 @@ export const MetaMask = () => {
         const [address] = await ethereum.request({
           method: "eth_requestAccounts",
         });
-        const chainId = await ethereum.request({ method: "eth_chainId" });
-
-        let contractAddress: string;
-        switch (chainId) {
-          case "0x1": // Mainnet
-            contractAddress = "";
-            break;
-          case "0x3": // Ropsten
-            contractAddress = "";
-            break;
-          case "0x4": // Rinkeby
-            contractAddress = "";
-            break;
-          case "0x89": // Polygon Mainnet
-            contractAddress = "";
-            break;
-          case "0x13881": // Polygon Testnet
-            contractAddress = "";
-            break;
-          default:
-            // Hardhat Local
-            contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-        }
-
+        let chainId = await ethereum.request({ method: "eth_chainId" });
+        chainId = Number(chainId);
         const signer = provider.getSigner(address);
         const account = signer._address;
 
-        let code;
-        try {
-          code = await signer.provider.getCode(contractAddress, "latest");
-          if (code == "0x") {
-            console.error(
-              `There is no code at ${contractAddress}. Please verify that the HBT contract was deployed at this address.`
-            );
-            displayToast(
-              "The HBT contract address provided does not correspond to a smart contract.",
-              {
-                type: "error",
-              }
-            );
-          }
-        } catch (error) {
-          console.log("error", error);
-        }
-
-        const contract = new ethers.Contract(
-          contractAddress,
-          MockHBT__factory.abi,
-          signer
-        ) as MockHBT;
+        const contract = await getHBTContract({
+          chainId,
+          signer,
+        });
 
         console.log(
           `Contract connected: ${
@@ -88,6 +49,7 @@ export const MetaMask = () => {
             contract,
             provider,
             account,
+            signer,
           }));
       } else if (window.web3) {
         console.log("Update MetaMask");
--- airdrop/frontend/src/context/Web3Context.tsx
+++ airdrop/frontend/src/context/Web3Context.tsx
@@ -1,54 +1,66 @@
 import type { Web3 } from "./NavBar/MetaMask";
 import { createContext, useState, useEffect } from "react";
+import { getHBTContract } from "src/utils/hbtContract";
 
 declare let window: any;
 
 export const Web3Context = createContext<Web3>(null!);
 
 export const Web3Provider: React.FC = ({ children }) => {
-	const [{ contract, provider, account }, setWeb3] = useState<Web3>({} as Web3);
+  const [{ contract, provider, account, signer }, setWeb3] = useState<Web3>(
+    {} as Web3
+  );
 
-	// Listens for network changes to reload the page
-	useEffect(() => {
-		window.ethereum.on("chainChanged", (chainId: string) =>
-			window.location.reload()
-		);
-		return () => {
-			window.ethereum.removeListener("chainChanged", (chainId: string) =>
-				window.location.reload()
-			);
-		};
-	}, []);
+  // Listens for network changes to reload the page
+  useEffect(() => {
+    window.ethereum.on("chainChanged", (chainId: string) =>
+      window.location.reload()
+    );
+    return () => {
+      window.ethereum.removeListener("chainChanged", (chainId: string) =>
+        window.location.reload()
+      );
+    };
+  }, []);
 
-	// Listens for a change in account and updates state
-	useEffect(() => {
-		function newAccount(accounts: Array<string>) {
-			const signer = provider?.getSigner(accounts[0]);
+  // Listens for a change in account and updates state
+  useEffect(() => {
+    async function newAccount(accounts: Array<string>) {
+      if (!provider) {
+        return;
+      }
+      const signer = provider.getSigner(accounts[0]);
+      const chainId = await signer.getChainId();
+      if (!contract) {
+        await getHBTContract({ signer, chainId });
+      }
 
-			setWeb3((prev: Web3) => ({
-				...prev,
-				contract: contract.connect(signer),
-				account: signer._address
-			}));
-		}
+      setWeb3((prev: Web3) => ({
+        ...prev,
+        signer,
+        contract: contract.connect(signer),
+        account: signer._address,
+      }));
+    }
 
-		window.ethereum.on("accountsChanged", newAccount);
-		return () => {
-			window.ethereum.removeListener("accountsChanged", newAccount);
-		};
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [account]);
+    window.ethereum.on("accountsChanged", newAccount);
+    return () => {
+      window.ethereum.removeListener("accountsChanged", newAccount);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  };, [account, provider]);
 
-	return (
-		<Web3Context.Provider
-			value={{
-				contract,
-				provider,
-				account,
-				setWeb3
-			}}
-		>
-			{children}
-		</Web3Context.Provider>
-	);
+  return (
+    <Web3Context.Provider
+      value={{
+        contract,
+        provider,
+        account,
+        signer,
+        setWeb3,
+      }}
+    >
+      {children}
+    </Web3Context.Provider>
+  );
 };
