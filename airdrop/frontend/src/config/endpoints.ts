95e83b57c67ab3cf58c972a7a9a0b641225855ae
ra-phael <10075759+ra-phael@users.noreply.github.com>
2022-10-31 18:39:13 +0100

    add new endpoints

--- airdrop/frontend/src/config/endpoints.ts
+++ airdrop/frontend/src/config/endpoints.ts
@@ -3,4 +3,7 @@ const baseURL = "http://localhost:" + port;
 
 export const BACKEND_ENDPOINTS = {
   eligible: (address: string) => baseURL + "/eligible/" + address,
+  siweNonce: baseURL + "/siwe/nonce",
+  siweVerify: baseURL + "/siwe/verify",
+  register: baseURL + "/register",
 };
