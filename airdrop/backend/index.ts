adbf978ba591b0a3e875b6a48564577cbeab9e10
ra-phael <10075759+ra-phael@users.noreply.github.com>
2022-10-31 18:34:49 +0100

    add siwe & register routes

--- airdrop/backend/index.ts
+++ airdrop/backend/index.ts
@@ -5,28 +5,43 @@ import cors from "cors";
 import helmet from "helmet";
 import hpp from "hpp";
 import mongoSanitize from "express-mongo-sanitize";
-
-import eligibleRouter from "./routes/eligible";
+import Session from "express-session";
+import { corsOrigin } from "./config/cors";
+import { eligibleRouter, registerRouter, siweRouter } from "./routes";
 
 const app = express();
 
 mongoose
-  .connect("mongodb://localhost:27017/blockchain")
+  .connect("mongodb://localhost:27017/hbt-airdrop")
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.error("FAILED to connect to MongoDB: " + err));
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: corsOrigin, credentials: true }));
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(mongoSanitize());
 app.use(hpp());
+app.use(
+  Session({
+    name: "hbt-airdrop-example",
+    // CHANGE ME
+    secret: "hbt-airdrop-example-secret",
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false, sameSite: true },
+  })
+);
 
 const port = process.env.PORT || 5000;
 
 app.use("/eligible", eligibleRouter);
+app.use("/siwe", siweRouter);
+app.use("/register", registerRouter);
 
 app.listen(port, () =>
-  console.log(`Backend server started on port ${port}...`)
+  console.log(
+    `Backend server started on port ${port} with environment: ${process.env?.NODE_ENV}.`
+  )
 );
---
+++ airdrop/backend/routes/register.ts
@@ -0,0 +1,75 @@
+import express, { Response } from "express";
+import {
+  ContainerTypes,
+  ValidatedRequest,
+  ValidatedRequestSchema,
+  createValidator,
+} from "express-joi-validation";
+import Joi from "joi";
+import { BackendErrors, EthAddress } from "../../shared/types";
+import { signedInWithEthereum } from "../middleware/siwe";
+import { ethAddressSchema } from "../models/address";
+import { hasRegisterSucceeded, register } from "../core/registration";
+
+interface RegisterAddressRequestSchema extends ValidatedRequestSchema {
+  [ContainerTypes.Body]: {
+    addressToRegister: EthAddress;
+    signature: string;
+  };
+}
+const registerAddressSchema = Joi.object({
+  addressToRegister: ethAddressSchema,
+  signature: Joi.string().required(),
+});
+
+const validator = createValidator();
+
+const router = express.Router();
+router.post(
+  "/",
+  signedInWithEthereum,
+  validator.body(registerAddressSchema),
+  async (
+    req: ValidatedRequest<RegisterAddressRequestSchema>,
+    res: Response
+  ) => {
+    const { addressToRegister, signature } = req.body;
+    const signedInAddress = req.session.siwe?.address;
+    console.log(`Registering ${addressToRegister}`);
+
+    // This should already be guaranteed by the middleware
+    if (!signedInAddress) {
+      res.status(400).send({ error: BackendErrors.NOT_SIGNED_IN });
+      return;
+    }
+    try {
+      const result = await register({
+        addressToRegister,
+        signature,
+        signedInAddress,
+      });
+
+      if (hasRegisterSucceeded(result)) {
+        return res.status(200).json({
+          registeredAddress: result.address,
+        });
+      }
+
+      if (result?.error) {
+        switch (result.error) {
+          case BackendErrors.INVALID_SIGNATURE_PROVIDED:
+            return res.status(400).send(result);
+          // TODO: handle other errors
+          default:
+            console.error(result);
+            return res.status(500).send();
+        }
+      }
+    } catch (error) {
+      console.error("Error while registering address", error);
+      return res.status(500).send();
+    }
+  }
+);
+
+export default router;
