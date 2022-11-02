e773e197368bb4fcea7edf72dd9699bbd28fc6eb
ra-phael <10075759+ra-phael@users.noreply.github.com>
2022-11-02 10:39:00 +0100

    improve homepage

--- airdrop/frontend/pages/index.tsx
+++ airdrop/frontend/pages/index.tsx
@@ -1,15 +1,31 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
-	return (
+  const router = useRouter();
+
+  const onRegisterClick = () => {
+    router.push("/eligible");
+  };
+
+  return (
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="text-center text-2xl">
-        Ready to participate in this airdrop, anon?
-      </h1>
+      <div className="text-center">
+        <h1 className="text-center text-2xl mb-4">
+          Ready to participate in this airdrop, anon?
+        </h1>
+        <h2 className="text-lg mb-4">
+          Check how many tokens your address is eligible for and register for
+          the upcoming airdrop.
+        </h2>
+        <button onClick={onRegisterClick} className="yellow-btn">
+          Register
+        </button>
+      </div>
     </>
   );
 };
