711299551b4c99ae895557ddeb303616a2638f23
ra-phael <10075759+ra-phael@users.noreply.github.com>
2022-11-02 10:44:58 +0100

    add flow to claim airdrop

--- airdrop/frontend/pages/claim.tsx
+++ airdrop/frontend/pages/claim.tsx
@@ -1,61 +1,23 @@
-import React, { useCallback, useContext, useEffect, useState } from "react";
-import { Web3Context } from "src/context/Web3Context";
-import { isAddressRegistered } from "src/services/registration";
-import { Registration } from "../../shared";
+import React from "react";
+import { ClaimBox } from "src/components/Claim";
+import { useAidrop } from "src/hooks/useAirdrop";
 
 export default function Claim() {
-  const { account } = useContext(Web3Context);
-  const [registration, setRegistration] = useState<Registration | null>(null);
-
-  const getRegistration = useCallback(async () => {
-    if (!account) return;
-
-    try {
-      const registration = await isAddressRegistered(account);
-      setRegistration(registration);
-    } catch (error) {
-      console.error(error);
-    }
-  }, [account]);
-
-  useEffect(() => {
-    getRegistration();
-  }, [getRegistration]);
-
-  const renderRegistration = (
-    registration: Registration | null,
-    account: string
-  ) => {
-    if (!registration) {
-      return;
-    }
-
-    if (!registration.isRegistered) {
-      return <p> {account} is not registered for the airdrop.</p>;
-    }
-
-    if (registration.isRegistered && registration.amount) {
-      return (
-        <div>
-          <p className="mb-4">
-            🚀 Your address qualifies for {registration.amount} tokens. 🚀
-          </p>
-          <button className="green-btn" onClick={() => console.log("Claiming")}>
-            Claim
-          </button>
-        </div>
-      );
-    }
-
-    return <p>Sorry, something went wrong.</p>;
-  };
+  const { account, registration, hasClaimed, claimAirdrop } = useAidrop();
 
   return (
     <div className="text-center">
       <h1 className="text-2xl my-4">Claim your airdrop</h1>
-      {!account
-        ? "Please connect your wallet"
-        : renderRegistration(registration, account)}
+      {!account ? (
+        "Please connect your wallet"
+      ) : (
+        <ClaimBox
+          registration={registration}
+          account={account}
+          hasClaimed={hasClaimed}
+          onClaim={claimAirdrop}
+        />
+      )}
     </div>
   );
 }
---
+++ airdrop/frontend/src/hooks/useAirdrop.ts
@@ -0,0 +1,105 @@
+import { useState, useEffect, useContext, useCallback } from "react";
+import {
+  isAddressNotRegistered,
+  Registration,
+  RegistrationRegistered,
+  sharedConfig,
+} from "../../../shared";
+import { Web3Context } from "src/context/Web3Context";
+import { isAddressRegistered } from "src/services/registration";
+import { Contract, ethers } from "ethers";
+import {
+  ERC20MerkleDrop,
+  ERC20MerkleDrop__factory,
+} from "../../lib/typechain-types";
+import { verifyAddressIsASmartContract } from "src/utils";
+
+export const useAidrop = () => {
+  const { account, signer } = useContext(Web3Context);
+  const [registration, setRegistration] = useState<Registration | null>(null);
+  const [hasClaimed, setHasClaimed] = useState<boolean | null>(null);
+  const [merkleDropContract, setMerkleDropContract] = useState<Contract | null>(
+    null
+  );
+
+  useEffect(() => {
+    if (!signer) return;
+
+    const getMerkleDropContract = async (): Promise<Contract> => {
+      const contractAddress = sharedConfig.merkleDropContractAddress;
+
+      await verifyAddressIsASmartContract(contractAddress, signer);
+
+      return new ethers.Contract(
+        contractAddress,
+        ERC20MerkleDrop__factory?.abi,
+        signer
+      );
+    };
+
+    getMerkleDropContract().then((contract) => {
+      if (!contract) {
+        console.error("Could not instantiate Merkle Drop contract.");
+        return;
+      }
+      setMerkleDropContract(contract);
+    });
+  }, [signer, account]);
+
+  const getRegistration = useCallback(async () => {
+    if (!account) return;
+    try {
+      const registration = await isAddressRegistered(account);
+      setRegistration(registration);
+    } catch (error) {
+      console.error(error);
+    }
+  }, [account]);
+
+  useEffect(() => {
+    getRegistration();
+  }, [getRegistration]);
+
+  const checkIfClaimed = useCallback(async () => {
+    const index = (registration as RegistrationRegistered)?.index;
+
+    const hasClaimed = await (merkleDropContract as ERC20MerkleDrop).isClaimed(
+      index
+    );
+
+    setHasClaimed(hasClaimed);
+  }, [registration, merkleDropContract]);
+
+  useEffect(() => {
+    if (
+      !registration ||
+      isAddressNotRegistered(registration) ||
+      !merkleDropContract
+    ) {
+      return;
+    }
+
+    checkIfClaimed();
+  }, [merkleDropContract, account, registration, checkIfClaimed]);
+
+  const claimAirdrop = useCallback(async () => {
+    if (registration == null || isAddressNotRegistered(registration)) {
+      console.log("Trying to claim but registration is not valid.");
+      return;
+    }
+    const { address, amount, proof } = registration;
+    console.log(
+      `Redeeming with address: ${address}, amount: ${amount}, proof: ${proof}.`
+    );
+    const tx = await (merkleDropContract as ERC20MerkleDrop).redeem(
+      address,
+      amount,
+      proof
+    );
+    await tx.wait();
+
+    checkIfClaimed();
+  }, [merkleDropContract, registration, checkIfClaimed]);
+
+  return { account, registration, hasClaimed, claimAirdrop } as const;
+};
