9f73e5a38b8f725a2c07bd9cdae9cfb8138095f4
ra-phael <10075759+ra-phael@users.noreply.github.com>
2022-11-03 16:29:14 +0100

    small fix

--- airdrop/backend/core/hbt.ts
+++ airdrop/backend/core/hbt.ts
@@ -7,13 +7,17 @@ import {
 } from "../../shared";
 import { ethers } from "ethers";
 
+const getLocalHBTContractAddress = () =>
+  humanboundContracts[Web3ChainReference.EIP155_HARDHAT_LOCAL].address;
+
 export const getHBTId = async (address: EthAddress): Promise<null | string> => {
   if (isDevelopment()) {
     var url = "http://localhost:8545";
     var customProvider = ethers.providers.getDefaultProvider(url);
     await customProvider.ready;
+
     const mockHBT = await MockHBT__factory.connect(
-      humanboundContracts[Web3ChainReference.EIP155_HARDHAT_LOCAL].address,
+      getLocalHBTContractAddress(),
       customProvider
     );
     const balance = await mockHBT.balanceOf(address);
