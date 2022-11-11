91ef6b927abf07132e460f2840ddc84ed5c1c4b1
Papa Smurf <chris@violet.co>
2022-11-11 12:57:34 +0000

    added wrap and unwrap functionalities to cERC20

--- cToken/contracts/contracts/cERC20.sol
+++ cToken/contracts/contracts/cERC20.sol
@@ -8,13 +8,32 @@ import "./IHumanboundToken.sol";
 contract cERC20 is ERC20, Ownable {
 	address humanboundToken;
 
+	address noncompliantERC20;
+	uint256 public tokensWrapped;
+
 	modifier onlyHumanboundHolder(address account) {
 		require(IHumanboundToken(humanboundToken).balanceOf(account) > 0, "cERC20: account is not humanbound token holder");
 		_;
 	}
 
-	constructor(string memory name_, string memory symbol_, address humanboundToken_) ERC20(name_, symbol_) Ownable() {
+	constructor(string memory name_, string memory symbol_, address humanboundToken_, address noncompliantERC20_) ERC20(name_, symbol_) Ownable() {
 		humanboundToken = humanboundToken_;
+		noncompliantERC20 = noncompliantERC20_;
+	}
+
+	function wrap(uint256 amount) public virtual onlyHumanboundHolder(msg.sender) {
+		IERC20(noncompliantERC20).transferFrom(msg.sender, address(this), amount);
+		tokensWrapped += amount;
+
+		_mint(msg.sender, amount);
+	}
+
+	function unwrap(uint256 amount) public virtual onlyHumanboundHolder(msg.sender) {
+		require(tokensWrapped >= amount, "cERC20_unwrap: amount to unwrap exceeds total wrapped");
+		_burn(msg.sender, amount);
+
+		IERC20(noncompliantERC20).transfer(msg.sender, amount);
+		tokensWrapped -= amount;
 	}
 
     function _transfer(
--- cToken/contracts/test/cERC20.test.ts
+++ cToken/contracts/test/cERC20.test.ts
@@ -4,7 +4,9 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 
 import { CERC20, MockHBT } from "../src/types/contracts";
+import { MockERC20 } from "../src/types/contracts/MockERC20";
 import { CERC20__factory, MockHBT__factory } from "../src/types/factories/contracts";
+import { MockERC20__factory } from "../src/types/factories/contracts/MockERC20__factory";
 
 describe("cERC20", function () {
   const oneEther = ethers.utils.parseEther("1");
@@ -18,14 +20,17 @@ describe("cERC20", function () {
     other: SignerWithAddress,
     spender: SignerWithAddress,
     mockHBT: MockHBT,
+    mockERC20: MockERC20,
     cERC20: CERC20;
 
   beforeEach(async function () {
     [owner, user, other, spender] = await ethers.getSigners();
     const MockHBTFactory = new MockHBT__factory(owner);
+    const ERC20Factory = new MockERC20__factory(owner);
     const CERC20Factory = new CERC20__factory(owner);
     mockHBT = await MockHBTFactory.deploy();
-    cERC20 = await CERC20Factory.deploy(tokenName, tokenSymbol, mockHBT.address);
+    mockERC20 = await ERC20Factory.deploy("ERC20", "ERC20");
+    cERC20 = await CERC20Factory.deploy(tokenName, tokenSymbol, mockHBT.address, mockERC20.address);
   });
 
   it("should be deployed correctly", async () => {
@@ -194,6 +199,85 @@ describe("cERC20", function () {
           );
           expect(await cERC20.callStatic.balanceOf(user.address)).to.equal(amount);
           expect(await cERC20.callStatic.balanceOf(other.address)).to.equal(0);
+        });
+      });
+    });
+  });
+
+  describe("wrap", () => {
+    context("uncompliant token", () => {
+      beforeEach(async () => {
+        await expect(mockERC20.mint(user.address, 10)).to.not.be.reverted;
+        expect(await mockERC20.balanceOf(user.address)).to.equal(amount);
+        await mockERC20.connect(user).approve(cERC20.address, ethers.constants.MaxUint256);
+
+        await expect(mockERC20.mint(other.address, 10)).to.not.be.reverted;
+        expect(await mockERC20.balanceOf(other.address)).to.equal(amount);
+        await mockERC20.connect(other).approve(cERC20.address, ethers.constants.MaxUint256);
+      });
+
+      context("from hbt owner", () => {
+        beforeEach(async () => {
+          await mockHBT.safeMint(user.address, hbtId);
+          expect(await mockHBT.callStatic.ownerOf(hbtId)).to.equal(user.address);
+        });
+
+        it("should successfully wrap", async () => {
+          await expect(cERC20.connect(user).wrap(amount)).to.not.be.reverted;
+
+          expect(await cERC20.callStatic.balanceOf(user.address)).to.equal(amount);
+          expect(await mockERC20.callStatic.balanceOf(user.address)).to.equal(0);
+        });
+
+        it("should fail to wrap with not enough funds", async () => {
+          await expect(cERC20.connect(user).wrap(amount + 1)).to.be.revertedWith(
+            "ERC20: transfer amount exceeds balance",
+          );
+        });
+      });
+
+      context("from non-hbt owner", () => {
+        it("should fail to wrap", async () => {
+          await expect(cERC20.connect(other).wrap(amount)).to.be.revertedWith(
+            "cERC20: account is not humanbound token holder",
+          );
+        });
+      });
+    });
+  });
+
+  describe("unwrap", () => {
+    context("wrapped tokens", () => {
+      beforeEach(async () => {
+        await expect(mockERC20.mint(user.address, 10)).to.not.be.reverted;
+
+        await expect(mockHBT.safeMint(user.address, hbtId)).to.not.be.reverted;
+
+        await mockERC20.connect(user).approve(cERC20.address, ethers.constants.MaxUint256);
+        await expect(cERC20.connect(user).wrap(amount)).to.not.be.reverted;
+      });
+
+      context("from hbt owner", () => {
+        it("should successfully unwrap", async () => {
+          await expect(cERC20.connect(user).unwrap(amount)).to.not.be.reverted;
+
+          expect(await cERC20.callStatic.balanceOf(user.address)).to.equal(0);
+          expect(await mockERC20.callStatic.balanceOf(user.address)).to.equal(amount);
+        });
+      });
+
+      context("from non-hbt owner", () => {
+        beforeEach(async () => {
+          await expect(mockHBT.burn(hbtId)).to.not.be.reverted;
+        });
+
+        it("should fail to unwrap", async () => {
+          await expect(cERC20.connect(user).unwrap(amount)).to.be.revertedWith(
+            "cERC20: account is not humanbound token holder",
+          );
+
+          expect(await cERC20.callStatic.balanceOf(user.address)).to.equal(amount);
+          expect(await mockERC20.callStatic.balanceOf(user.address)).to.equal(0);
         });
       });
     });
