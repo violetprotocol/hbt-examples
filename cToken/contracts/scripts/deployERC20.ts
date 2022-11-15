105cda0566c3effeb0e291dbfe00abc0db2861ae
Papa Smurf <chris@violet.co>
2022-11-15 21:01:04 +0000

    updated scripts to deploy mock erc20 instead

--- cToken/contracts/scripts/deployERC20.ts
+++ cToken/contracts/scripts/deployERC20.ts
@@ -7,7 +7,7 @@ import hre from "hardhat";
 
 async function main() {
   // We get the contract to deploy
-  const ERC20 = await hre.ethers.getContractFactory("ERC20");
+  const ERC20 = await hre.ethers.getContractFactory("MockERC20");
   const erc20 = await ERC20.deploy("Test ERC20", "ERC20");
 
   await erc20.deployed();
--- cToken/contracts/scripts/deploycERC20.ts
+++ cToken/contracts/scripts/deploycERC20.ts
@@ -6,7 +6,7 @@
 import hre from "hardhat";
 
 const HBTAddress = "0x581af8eaEa059cAA65A2652321aB7e2Cd3a4d423";
-const ERC20Address = "0xd66d4Fa10270Ed8DFfDB6f4c5bA3AE0e1c3DD0e6";
+const ERC20Address = "0x99699724dDe872ae816Ce480775FDFce2D3fC9d4";
 
 async function main() {
   // We get the contract to deploy
