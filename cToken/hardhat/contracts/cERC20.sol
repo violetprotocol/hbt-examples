4cc4a22f18210c71c0af78b04449f3a0cf9f77b6
Papa Smurf <chris@violet.co>
2022-11-17 12:44:15 +0000

    added comments to contract

--- cToken/hardhat/contracts/cERC20.sol
+++ cToken/hardhat/contracts/cERC20.sol
@@ -6,13 +6,19 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./IHumanboundToken.sol";
 
 /**
+ * Compliant ERC20 Token
+ * Example implementation of using Humanbound Token to gate token access to verified users
+ * 
+ * @dev `onlyHumanboundHolder` is the key modifier to be used in your token
+ * @notice The below implementation gives an example of how to use the `onlyHumanboundHolder` modifier to restrict
+ * usage of the token. This can be applied to ERC20, ERC223, ERC721 among many other token standards to achieve a
+ * compliant token.
+ *
+ * Example use cases include compliant stablecoins (cUSDC, cDAI), compliant NFT drops, compliant governance tokens etc.
  */
 
 contract cERC20 is ERC20 {
 	address humanboundToken;
-
-	address noncompliantERC20;
-	uint256 public tokensWrapped;
 
 	modifier onlyHumanboundHolder(address account) {
 		require(IHumanboundToken(humanboundToken).balanceOf(account) > 0, "cERC20: account is not humanbound token holder");
@@ -24,6 +30,30 @@ contract cERC20 is ERC20 {
 		noncompliantERC20 = noncompliantERC20_;
 	}
 
+    function _transfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override onlyHumanboundHolder(to) onlyHumanboundHolder(from) {
+        super._transfer(from, to, amount);
+    }
+
+    // Features for a standard minting flow for cERC20
+	// Simple unpermissioned minting of new cERC20 tokens that can only be minted to HBT holders
+	function _mint(address account, uint256 amount) internal virtual override onlyHumanboundHolder(account) {
+        super._mint(account, amount);
+    }
+
+	function mint(address account, uint256 amount) public virtual {
+		_mint(account, amount);
+	}
+
+    // Features for a wrapping/unwrapping flow
+	// Takes a non-compliant ERC20 token, and converts it to compliant cERC20 through wrapping process
+	// Wrapping can only be performed by HBT holders
+	address noncompliantERC20;
+	uint256 public tokensWrapped;
+	
 	function wrap(uint256 amount) public virtual onlyHumanboundHolder(msg.sender) {
 		IERC20(noncompliantERC20).transferFrom(msg.sender, address(this), amount);
 		tokensWrapped += amount;
@@ -37,21 +67,5 @@ contract cERC20 is ERC20 {
 
 		IERC20(noncompliantERC20).transfer(msg.sender, amount);
 		tokensWrapped -= amount;
-	}
-
-    function _transfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal virtual override onlyHumanboundHolder(to) onlyHumanboundHolder(from) {
-        super._transfer(from, to, amount);
-    }
-	
-	function _mint(address account, uint256 amount) internal virtual override onlyHumanboundHolder(account) {
-        super._mint(account, amount);
-    }
-
-	function mint(address account, uint256 amount) public virtual {
-		_mint(account, amount);
 	}
 }
