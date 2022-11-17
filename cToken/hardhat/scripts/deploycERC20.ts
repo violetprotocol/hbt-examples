6daccfa9709f881d579c0d0a751f9306fd1f4f42
Papa Smurf <chris@violet.co>
2022-11-17 17:41:13 +0000

    fixed script that deploys and prewraps ERC20

--- cToken/hardhat/scripts/deploycERC20.ts
+++ cToken/hardhat/scripts/deploycERC20.ts
@@ -5,8 +5,8 @@
 // Runtime Environment's members available in the global scope.
 import hre from "hardhat";
 
-const HBTAddress = "0x581af8eaEa059cAA65A2652321aB7e2Cd3a4d423";
-const ERC20Address = "0x99699724dDe872ae816Ce480775FDFce2D3fC9d4";
+const HBTAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const ERC20Address = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 const premintERC20 = 1000000;
 
 async function main() {
@@ -18,9 +18,15 @@ async function main() {
 
   console.log("cERC20 Token deployed to:", cERC20.address);
 
+  const signer = (await hre.ethers.getSigners())[0].address;
+
   // To enable smooth unwrapping flows, we will premint and prewrap tokens for demonstration purposes
   const ERC20 = await hre.ethers.getContractAt("MockERC20", ERC20Address);
-  let tx = await ERC20.mint((await hre.ethers.getSigners())[0].address, premintERC20);
+  let tx = await ERC20.mint(signer, premintERC20);
+  await tx.wait();
+
+  const MockHBT = await hre.ethers.getContractAt("MockHBT", HBTAddress);
+  tx = await MockHBT.safeMint(signer, 0);
   await tx.wait();
 
   tx = await ERC20.approve(cERC20.address, premintERC20);
