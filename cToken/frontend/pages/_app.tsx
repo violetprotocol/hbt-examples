9bc5b1f42e08772f4fabb15cb3836f84faa22f66
Papa Smurf <chris@violet.co>
2022-11-22 16:54:27 +0000

    removed web3contextprovider, updated gitignore for typechain types

--- cToken/frontend/pages/_app.tsx
+++ cToken/frontend/pages/_app.tsx
@@ -4,32 +4,29 @@ import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import "react-toastify/dist/ReactToastify.css";
 import { RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
-import { WagmiConfig, allChains, configureChains, createClient } from "wagmi";
 
-import { Web3Provider } from "src/context/Web3Context";
+import { WagmiConfig } from "wagmi";
 import Layout from "src/context/Layout";
 import { RefreshContextProvider } from "src/context/RefreshContext";
 import { chains, wagmiClient } from "src/utils/wagmi";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Web3Provider>
-      <WagmiConfig client={wagmiClient}>
-        <RainbowKitProvider
-          chains={chains}
-          modalSize="compact"
-          theme={darkTheme({
-            accentColor: "#A73CFD",
-          })}
-        >
-          <RefreshContextProvider>
-            <Layout>
-              <Component {...pageProps} />
-            </Layout>
-          </RefreshContextProvider>
-        </RainbowKitProvider>
-      </WagmiConfig>
-    </Web3Provider>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider
+        chains={chains}
+        modalSize="compact"
+        theme={darkTheme({
+          accentColor: "#A73CFD",
+        })}
+      >
+        <RefreshContextProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </RefreshContextProvider>
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
 
--- cToken/frontend/src/context/Layout.tsx
+++ cToken/frontend/src/context/Layout.tsx
@@ -1,7 +1,5 @@
 import { NavBar } from "./NavBar/NavBar";
 import Head from "next/head";
-import { Web3Context } from "./Web3Context";
-import { useContext } from "react";
 import ConnectWallet from "src/components/ConnectWallet";
 import { useAccount } from "wagmi";
 
